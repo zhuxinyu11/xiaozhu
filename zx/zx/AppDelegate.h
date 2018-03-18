b30ba4c89b2e98892575965505ac093af1ea7549
xiaozhu <409022813@qq.com>
2018-03-18 16:14:40 +0800

    初始化项目

---
+++ zx/zx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zx
+//
+//  Created by 朱新余 on 2018/3/18.
+//  Copyright © 2018年 朱新余. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zx/zx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zx
+//
+//  Created by 朱新余 on 2018/3/18.
+//  Copyright © 2018年 朱新余. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
